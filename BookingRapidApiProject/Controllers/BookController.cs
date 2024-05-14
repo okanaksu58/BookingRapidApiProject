a7a259cc96d093fc317303ba39462b8fcb28ff09
OKAN AKSU <okanaksu_58@hotmail.com>
2024-05-14 16:49:46 +0300

    ..

--- BookingRapidApiProject/Controllers/BookController.cs
+++ BookingRapidApiProject/Controllers/BookController.cs
@@ -7,13 +7,13 @@ namespace BookingRapidApiProject.Controllers
 
     public class BookController : Controller
     {
-        public async Task<string> GetId(string city)
+        public async Task<string> GetId(string cityName)
         {
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Paris&locale=en-gb"),
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={cityName}&locale=tr"),
                 Headers =
     {
         { "X-RapidAPI-Key", "c8de957553mshf1f6c4611cb197bp1849e8jsn10e0418ee477" },
@@ -24,10 +24,10 @@ namespace BookingRapidApiProject.Controllers
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<City>>(body);
-                if (values != null && values.Count > 0)
+                var locations = JsonConvert.DeserializeObject<List<City>>(body);
+                if (locations != null && locations.Count > 0)
                 {
-                    return values[0].dest_id;
+                    return locations[0].dest_id;
                 }
                 else
                 {
@@ -43,7 +43,7 @@ namespace BookingRapidApiProject.Controllers
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/search?checkout_date=2024-09-15&order_by=popularity&filter_by_currency=AED&room_number=1&dest_id=-1456928&dest_type=city&adults_number=2&checkin_date=2024-09-14&locale=en-gb&units=metric&include_adjacency=true&children_number=2&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=0&children_ages=5%2C0"),
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/search?checkout_date=2024-09-15&order_by=popularity&filter_by_currency=AED&room_number=1&dest_id={p.dest_id}&dest_type=city&adults_number=2&checkin_date=2024-09-14&locale=en-gb&units=metric&include_adjacency=true&children_number=2&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=0&children_ages=5%2C0"),
                 Headers =
     {
         { "X-RapidAPI-Key", "c8de957553mshf1f6c4611cb197bp1849e8jsn10e0418ee477" },
@@ -83,7 +83,7 @@ namespace BookingRapidApiProject.Controllers
             return View("Index", hotels);
 
         }
-       
-        
+
+
     }
 }
